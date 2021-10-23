4d9337b72bb9389e0874b4c63e5c726a06869b7b
harkiratsm <multaniharry714@gmail.com>
2021-10-23 19:23:56 +0530

    Drawing the neural network

---
+++ src/Log.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Log() {
+    return (
+        <div>
+            Log displays here 
+        </div>
+    )
+}
+
+export default Log
---
+++ src/handpose.js
@@ -0,0 +1,1 @@
+import * as handpose from "@tensorflow-models/handpose";
